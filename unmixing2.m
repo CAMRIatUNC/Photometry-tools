952137349c70b1c1389df65c96b6cff3ec206d46
Tzu-Hao-Harry-Chao <73857632+Tzu-Hao-Harry-Chao@users.noreply.github.com>
2020-11-03 10:45:37 -0500

    Update unmixing2.m

--- unmixing2.m
+++ unmixing2.m
@@ -1,6 +1,7 @@
 function [] = unmixing2
 %%
 % Read raw mixed spectra time series from Oceanview output, and unmix the spectra time series one by one using user defined spectra reference.
+% Tzu-Hao Harry Chao 2020/02/14
 %%
 
 clc
