370588a9ade7a2000f73b3c61fecddb992951c16
Tzu-Hao-Harry-Chao <73857632+Tzu-Hao-Harry-Chao@users.noreply.github.com>
2021-10-21 15:22:49 -0400

    Update HRF_from_photometry_data.m

--- HRF_from_photometry_data.m
+++ HRF_from_photometry_data.m
@@ -1,15 +1,15 @@
-function [HRF]=HRF_from_photometry_data(neuronal_activity,BOLD,sampling_rate,HRF_length,pre)
+function [HRF]=HRF_from_photometry_data(neuronal_activity,hemodynamics,sampling_rate,HRF_length,pre)
 %%
-% calculate HRF via simultaneously recorded neuronal_activity and BOLD time
-% course.
-% neuronal_activity / BOLD: data recoreded in column
+% calculate HRF via simultaneously recorded neuronal_activity and
+% hemodynamics time-course.
+% neuronal_activity / hemodynamics: data recoreded in column
 % pre: 0 or any number if the neuronal_activity is considered as feedback
 % response and can be after the initiation of hemodynamic response.
 % Tzu-Hao Harry Chao 2020/07/04
 %%
 
 DA_zscore=(neuronal_activity-mean(neuronal_activity))/std(neuronal_activity);
-BOLD=BOLD-min(BOLD);
+hemodynamics=hemodynamics-min(hemodynamics);
 
 HRF_length=HRF_length*sampling_rate+1; % sec to data points
 pre=pre*sampling_rate; % sec to data points
@@ -25,5 +25,5 @@ end
 X(:,HRF_length+1)=ones(raw_data_length,1)';
 X(:,HRF_length+2)=linspace(0,1,raw_data_length)';
 
-HRF=pinv(X(pre+1:raw_data_length,:))*BOLD(1:raw_data_length-pre);
+HRF=pinv(X(pre+1:raw_data_length,:))*hemodynamics(1:raw_data_length-pre);
 HRF=HRF(1:HRF_length);
