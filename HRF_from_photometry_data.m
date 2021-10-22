dc32536c2c42b3f04e9fa382d3fef424e84fb040
Tzu-Hao-Harry-Chao <73857632+Tzu-Hao-Harry-Chao@users.noreply.github.com>
2021-10-22 11:23:52 -0400

    Update HRF_from_photometry_data.m

--- HRF_from_photometry_data.m
+++ HRF_from_photometry_data.m
@@ -1,14 +1,10 @@
-function [HRF]=HRF(neuronal_activity,hemodynamics,sampling_rate,HRF_length,pre)
+function [HRF]=HRF(neuronal_activity,hemodynamics,sampling_rate,HRF_length)
 %%
 % calculate HRF via simultaneously recorded neuronal_activity and
 % hemodynamics time-courses.
 %
 % neuronal_activity: neuronal activity time-course in a single column.
 % hemodynamics: hemodynamics changes time-course in a single column.
-% pre: use 0 in most of the case,
-%      unless you assume the recorded neuronal activity and hemodynamic 
-%      response are driven by a common source instead of under a causal 
-%      relationship from neuronal activity to hemodynamic response.
 %
 % Example: HRF(GCaMP_timecourse,Rhodamine_timecourse,10,25,0)
 % Tzu-Hao Harry Chao 2021/10/22
@@ -17,7 +13,6 @@ function [HRF]=HRF(neuronal_activity,hemodynamics,sampling_rate,HRF_length,pre)
 hemodynamics=hemodynamics-min(hemodynamics);
 
 HRF_length=HRF_length*sampling_rate+1; % sec to data points
-pre=pre*sampling_rate; % sec to data points
 
 clear X
 raw_data_length=length(neuronal_activity);
@@ -30,5 +25,5 @@ end
 X(:,HRF_length+1)=ones(raw_data_length,1)';
 X(:,HRF_length+2)=linspace(0,1,raw_data_length)';
 
-HRF=pinv(X(pre+1:raw_data_length,:))*hemodynamics(1:raw_data_length-pre);
+HRF=pinv(X(1:raw_data_length,:))*hemodynamics(1:raw_data_length);
 HRF=HRF(1:HRF_length);
